87bba5bd9fe81c59040eae8d3bf825c5d25fcbef
jorgedclopes <jorgedclopes@gmail.com>
2020-01-28 17:25:41 +0000

    Added getPiece function to the Board class.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import game.Board;
+import game.*;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -10,6 +12,17 @@ public class Main {
         try {
             Board board = new Board(8);
             System.out.print(board.OccupancyString());
+
+            /*
+            //This can be used to test Piece.getPossibleMoves;
+            Piece test = board.getPiece('W','Y');
+
+            System.out.println("Piece position: " + (char)('a'+test.getPlace().getPosition()[1])+ (test.getPlace().getPosition()[0]+1));
+            ArrayList<String> moveList = test.getPossibleMoves(board);
+            for(String move:moveList){
+                System.out.println(move);
+            }
+            */
         } catch (IOException e) {
             e.printStackTrace();
         }
